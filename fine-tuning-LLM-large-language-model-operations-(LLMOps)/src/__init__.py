92caa7aed1012360b85a4c52eb91f1cba9ea3f18
MisterFOURXXX <fourapiwit@gmail.com>
2026-09-18 10:11:26 +0200

    Re-structure repository

--- fine-tuning-LLM-large-language-model-operations-(LLMOps)/src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-LLMOps RAG Pipeline - Main Package
-"""
-
-__version__ = "1.0.0"
-__author__ = "MisterFOUR"
--- retrieval-augmented-generation-(RAG)-large-language-model-operations-(LLMOps)/src/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from src.model.load_model import load_fine_tuned_model
-from src.model.fine_tune import fine_tune_model
-from src.model.inference import ModelInference
-
-__all__ = ["load_fine_tuned_model", "fine_tune_model", "ModelInference"]
